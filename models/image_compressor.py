b27b1a92c61fb7f67f9120ae1580b40711162c8e
EsakaK <togelbian@gmail.com>
2022-07-07 12:57:34 +0800

    finish ImageCompressor

--- models/image_compressor.py
+++ models/image_compressor.py
@@ -12,34 +12,64 @@ class ImageCompressor(nn.Module):
         self.bit_estimator = BitsEstimator(192, K=5)
 
     def forward(self, inputs):
+        """
+        :param inputs: mini-batch
+        :return: rec_imgs: 重构图像  bits_map: 累计分布函数
+        """
         y = self.encoder(inputs)
-        bits_map = self.bit_estimator(y)
-        rec_imgs = self.decoder(y)
+        y_hat = self.quantize(y, is_train=True)
+        bits_map = self.bit_estimator(y_hat)
+        rec_imgs = self.decoder(y_hat)
 
         return bits_map, rec_imgs
 
-    def loss(self, loss_items):
-        bits_map, rec_img = loss_items
-        bit_rate = 0
-        distortion = 0
-        loss = bit_rate + 0.5 * distortion
-        return loss, bit_rate, distortion
+    def loss(self, inputs, loss_items, Lambda=0.5):
+        """
+        :param inputs: original images
+        :param loss_items: include bits_map and reconstruced images
+        :param Lambda: trade-off
+        :return:
+        """
+        bits_map, rec_imgs = loss_items
+        # R loss
+        total_bits = torch.sum(
+            torch.clamp(
+                (-torch.log(
+                    self.bit_estimator(bits_map + 0.5) - self.bit_estimator(bits_map - 0.5) + 1e-6)) / torch.log(
+                    torch.tensor(2.0)),
+                0,
+                50))
+        img_shape = rec_imgs.size()
+        bbp = total_bits / (img_shape[0] * img_shape[2] * img_shape[3])
+        # D loss
+        distortion = torch.mean((inputs - rec_imgs) ** 2)
+        # total loss
+        loss = total_bits + Lambda * distortion
+        return loss, bbp, distortion
 
-    def quantize(self, y):
-        y_hat = torch.round(y)
+    def quantize(self, y, is_train=False):
+        if is_train:
+            uniform_noise = nn.init.uniform_(torch.zeros_like(y), -0.5, 0.5)
+            if torch.cuda.is_available():
+                uniform_noise = uniform_noise.cuda()
+            y_hat = y + uniform_noise
+        else:
+            y_hat = torch.round(y)
         return y_hat
 
     def inference(self, img):
+        """
+        only use in test and validate
+        """
         y = self.encoder(img)
-        y = self.quantize(y)
-        rec_img = self.decoder(y)
+        y_hat = self.quantize(y, is_train=False)
+        rec_img = self.decoder(y_hat)
         return rec_img
+
 
 if __name__ == '__main__':
     i = ImageCompressor()
     i = i.cuda()
-    inputs = torch.randn((8,3,256,256)).cuda()
-    print(next(i.parameters()).device)
-    print(inputs.device)
-    bits_map, rec_imgs = i(inputs)
-    print(bits_map.size())
+    input_image = torch.randn((8, 3, 256, 256)).cuda()
+    rec_img = i.inference(input_image)
+    print(input_image)
